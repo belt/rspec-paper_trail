e6d1d87659056f7a392d7f9cca8574842d275c1a
Paul Belt <saprb@channing.harvard.edu>
2013-04-10 14:38:53 -0400

    dummy app for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,22 +1,33 @@
 require 'rubygems'
 require 'bundler/setup'
 
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../dummy/config/environment', __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+
 require 'rails'
 require 'active_record'
 require 'paper_trail'
 require 'rspec/paper_trail'
 
-ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-ActiveRecord::Schema.define(:version => 1) do
+RSpec.configure do |config|
+  config.order = 'random'
+end
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
+
+ActiveRecord::Schema.define(version: 1) do
   create_table :models do |t|
     t.string :name
     t.string :color
   end
   create_table :versions do |t|
-    t.string   :item_type, :null => false
-    t.integer  :item_id,   :null => false
-    t.string   :event,     :null => false
+    t.string   :item_type, null: false
+    t.integer  :item_id,   null: false
+    t.string   :event,     null: false
     t.string   :whodunnit
     t.text     :object
     t.datetime :created_at
